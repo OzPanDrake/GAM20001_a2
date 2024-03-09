d3c7fa8ab3058e810e789853c80e05dd97b79168
Blair Rod <103892145@student.swin.edu.au>
2024-03-10 00:54:50 +1100

    added main menu button
    added return to main menu button to cell and court scenes

--- game/cell.gd
+++ game/cell.gd
@@ -4,3 +4,7 @@ extends Node2D
 
 func _on_court_button_pressed():
 	get_tree().change_scene_to_file("res://court.tscn")
+
+
+func _on_main_button_pressed():
+	get_tree().change_scene_to_file("res://main.tscn")
--- game/court.gd
+++ game/court.gd
@@ -4,3 +4,7 @@ extends Node2D
 
 func _on_cell_button_pressed():
 	get_tree().change_scene_to_file("res://cell.tscn")
+
+
+func _on_main_button_pressed():
+	get_tree().change_scene_to_file("res://main.tscn")
