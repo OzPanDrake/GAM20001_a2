814a92ffbf3ba4b55fd836dc750f62931734a229
Blair Rod <103892145@student.swin.edu.au>
2024-03-10 04:32:08 +1100

    testing timers
    couldn't get to work, deleted timers

--- game/scripts/cell.gd
+++ game/scripts/cell.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 
-
 func _on_court_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/court.tscn")
 
--- game/scripts/main.gd
+++ game/scripts/main.gd
@@ -1,9 +1,7 @@
 extends Node2D
 
-func _on_quit_button_pressed():
-	get_tree().quit()
-
-
-
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/cell.tscn")
+
+func _on_quit_button_pressed():
+	get_tree().quit()
