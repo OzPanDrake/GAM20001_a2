5023f9cd6087d1c1c7c40d13a60ba48d8359c048
Blair Rod <103892145@student.swin.edu.au>
2024-03-11 00:16:41 +1100

    added splash screen and cell scene timer
    splach screen shows for 1 second, contains basic info.
    timer on cell screeen is 500 seconds  formatted in displayed as hh:mm:ss

--- game/scripts/cell.gd
+++ game/scripts/cell.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+func _process(_delta: float) -> void:
+	$TimerLabel.text = "Time Remaining: " + "%s" %  GetTime()
 
 func _on_court_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/court.tscn")
@@ -7,3 +9,14 @@ func _on_court_button_pressed():
 
 func _on_main_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+func _on_countdown_timer_timeout():
+	get_tree().change_scene_to_file("res://scenes/court.tscn")
+
+func GetTime():
+	var time = round($CountdownTimer.time_left)
+	var seconds = int(time)%60
+	var minutes = (int(time)/60)%60
+	var hours = (int(time)/60)/60
+	return "%02d:%02d:%02d" % [hours, minutes, seconds]
