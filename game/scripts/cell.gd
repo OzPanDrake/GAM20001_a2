6417e5d52dbed177c1ad0a5dc858cb99baa1216e
Blair Rod <103892145@student.swin.edu.au>
2024-03-14 03:51:53 +1100

    rng voice location
    added 8 locations for voices, randomiser plays different location

--- game/scripts/cell.gd
+++ game/scripts/cell.gd
@@ -3,11 +3,16 @@ extends Node2D
 @onready var cd_timer : Timer = $CountdownTimer
 @onready var voice_timer : Timer = $SoundTimer
 
+var voices_array = []
+var rng = RandomNumberGenerator.new()
+var index
+
 func _ready() -> void:
 	cd_timer.wait_time = 300
 	cd_timer.start()
-	voice_timer.wait_time = 3
+	voice_timer.wait_time = 2
 	voice_timer.start()
+	voices_array = [$Voices/voice1/AudioStreamPlayer2D, $Voices/voice2/AudioStreamPlayer2D, $Voices/voice3/AudioStreamPlayer2D, $Voices/voice4/AudioStreamPlayer2D, $Voices/voice5/AudioStreamPlayer2D, $Voices/voice6/AudioStreamPlayer2D, $Voices/voice7/AudioStreamPlayer2D, $Voices/voice8/AudioStreamPlayer2D]
 
 func _process(_delta: float) -> void:
 	$TimerLabel.text = "Time Remaining: " + "%s" %  GetTime()
@@ -26,4 +31,7 @@ func GetTime():
 	return "%02d:%02d:%02d" % [hours, minutes, seconds]
 
 func _on_sound_timer_timeout() -> void:
-	$voice_location/AudioStreamPlayer2D.play()
+	rng.randomize()
+	index = rng.randi_range(0, 7)
+	voices_array[index].play()
+	print_debug(index)
