dba36a139bff1a187f74d703b2f8b99934c11401
OzPanDrake <80376364+OzPanDrake@users.noreply.github.com>
2024-04-15 22:42:14 +1000

    Timer for projectiles

--- game/scenes/projectile.gd
+++ game/scenes/projectile.gd
@@ -4,9 +4,19 @@ class_name Projectile
 
 var speed: float = 15.0
 var damage: float = 30.0
+var time_to_live: float =  0.4
 
 func _ready() -> void:
 	body_entered.connect(on_body_entered)
+	handle_time_to_live()
+
+func handle_time_to_live() -> void:
+	var ttl_timer: Timer = Timer.new()
+	add_child(ttl_timer)
+	ttl_timer.wait_time = time_to_live
+	ttl_timer.one_shot = true
+	ttl_timer.timeout.connect(func(): queue_free())
+	ttl_timer.start()
 
 func _physics_process(delta: float) -> void:
 	position += transform.x * speed
